3acad11858b70c52d2b22276d222f02767e49ab5
robino5 <anamulrobin05@gmail.com>
2023-01-15 23:16:14 +0600

    desig

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import swal from 'sweetalert'
-import axios from 'axios'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import axios from "axios";
 import {
   CButton,
   CFormLabel,
@@ -11,52 +11,54 @@ import {
   CContainer,
   CFormInput,
   CRow,
-} from '@coreui/react'
+  CImage,
+} from "@coreui/react";
+import logo from "src/assets/images/logo.jpeg";
 
 const Login = () => {
-  const navigate = useNavigate()
-  const [username, setuername] = useState('')
-  const [password, setpassword] = useState('')
+  const navigate = useNavigate();
+  const [username, setuername] = useState("");
+  const [password, setpassword] = useState("");
 
   const handleUser = (e) => {
-    const username = e.target.value
-    setuername(username)
-  }
+    const username = e.target.value;
+    setuername(username);
+  };
   const handlePassword = (e) => {
-    const password = e.target.value
-    setpassword(password)
-  }
+    const password = e.target.value;
+    setpassword(password);
+  };
 
   const submitUser = (e) => {
-    e.preventDefault()
-    var data = new FormData()
-    data.append('username', username)
-    data.append('password', password)
+    e.preventDefault();
+    var data = new FormData();
+    data.append("username", username);
+    data.append("password", password);
     axios
       .post(`${process.env.REACT_APP_API_URL}mruser-auth/token`, data)
       .then((response) => {
-        console.log(response)
-        localStorage.setItem('token', response.data.access_token)
+        console.log(response);
+        localStorage.setItem("token", response.data.access_token);
         swal({
-          position: 'top-end',
-          text: 'Login Successfull',
-          icon: 'success',
+          position: "top-end",
+          text: "Login Successfull",
+          icon: "success",
           button: false,
           timer: 1500,
-        })
-        return navigate('/statement')
+        });
+        return navigate("/statement");
       })
       .catch((error) => {
-        console.error('There was an error!', error)
+        console.error("There was an error!", error);
         swal({
-          position: 'top-end',
+          position: "top-end",
           text: error.response.data.detail,
-          icon: 'error',
+          icon: "error",
           button: false,
           timer: 1500,
-        })
-      })
-  }
+        });
+      });
+  };
 
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
@@ -68,7 +70,10 @@ const Login = () => {
               <CCardBody>
                 <div className="justify-content-center px-3">
                   <div className="text-center">
-                    <h4 className="mt-1 mb-5 pb-1">Login Form</h4>
+                    <CImage className="login-image-wrapper" src={logo} />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="mt-1  pb-1">Merchant Login</h4>
                   </div>
                   {/* <CFormLabel>Merchant Id</CFormLabel>
                   <FormControl
@@ -93,7 +98,10 @@ const Login = () => {
                     onChange={handlePassword}
                   />
                   <div className="text-center pt-1 mb-2 pb-1">
-                    <CButton className="mb-4 w-100 gradient-custom-2" onClick={submitUser}>
+                    <CButton
+                      className="mb-4 w-100 gradient-custom-2"
+                      onClick={submitUser}
+                    >
                       Sign in
                     </CButton>
                     {/* <a className="text-muted" href="#!">
@@ -108,7 +116,7 @@ const Login = () => {
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
